9e1495a620a1914f70fced37405e4abf666eb5ab
Ronan Barzic <rbarzic@gmail.com>
2015-04-08 23:26:56 +0200

    First version to compile and link

---
+++ Makefile
@@ -0,0 +1,47 @@
+PROJECT_NAME=test
+
+C_SOURCE_FILES := $(wildcard src/*.c)
+
+SRCS_AS :=
+
+INC_PATHS  := -I.
+
+DEVICE = NRF51
+#CHIP=nRF51822-QFAA
+#CHIP=nRF51822-CEAA
+#CHIP=nRF51822-QFAB
+#CHIP=nRF51822-CDAB
+#CHIP=nRF51822-QFAC
+#CHIP=nRF51822-CFAC
+
+
+#CHIP=nRF51422-QFAA
+#CHIP=nRF51422-CEAA
+#CHIP=nRF51422-QFAB
+#CHIP=nRF51422-CDAB
+CHIP=nRF51422-QFAC
+#CHIP=nRF51422-CFAC
+# From nRF51_Series_Compatibility_Matrix_v1.2.pdf :
+#  The nRF51-DK and nRF51-Dongle is using the nRF51422-QFAC variant of the chip
+BOARD =  nRF51_DK # Where is the nRF51 board file ?
+
+SDK_VERSION=nRF51_SDK_7.2.0_cf547b5
+SDK_URL=http://developer.nordicsemi.com/nRF51_SDK
+
+SDK_INSTALL_DIR=../../nordic
+USE_SOFT_DEVICE=no
+
+
+# list SDK modules used (see misc/SDK_Makefile.mk)
+
+# Startup code
+SDK_TOOLCHAIN_GCC=yes
+
+SDK_DRIVERS_NRF_HAL=yes
+
+
+
+# Don't touch lines below
+include misc/nRF51_Devices.mk
+include misc/SDK_Makefile.mk
+include misc/Makefile
---
+++ misc/get_libraries.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import os
+import argparse
+import fnmatch
+import pprint as pp
+
+
+def get_args():
+    """
+    Get command line arguments
+    """
+
+    parser = argparse.ArgumentParser(description="""
+Put description of application here
+                   """)
+    parser.add_argument('--dir', action='store', dest='dir',
+                        help='directory to look into')
+
+    parser.add_argument('--pattern', action='store', dest='pattern',
+                        help='pattern (Unix style pattern matching)  to be matched')
+
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+
+    return parser.parse_args()
+
+
+def find_files(dir, pattern):
+    for root, dirs, files in os.walk(dir):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = os.path.join(root, basename)
+                yield filename
+
+
+allowed_top_dirs = ['components']
+
+if __name__ == '__main__':
+    args = get_args()
+    modules = {}
+    includes = {}
+    asm_files = {}
+
+    sdk_top = args.dir.split('/')[-1]
+    top_length = len(args.dir.split('/'))
+    # print "SDK name/top ", sdk_top
+    for f in find_files(args.dir, args.pattern):
+        #print "Full path  ", f
+        relative_path = f.replace(args.dir, '$(SDK_INSTALL_DIR)/$(SDK_VERSION)')
+        fa = f.split('/')
+        file_name = fa[-1]
+        submodule = fa[-2]
+        module = fa[-3]
+        top_dir = fa[top_length]
+        extension = os.path.splitext(file_name)[1][1:].strip().lower()
+        #base2  = fa[top_length+1]
+
+        #print "File", file_name
+        #print "Submodule", submodule
+        #print "Module ", module
+        # print "Base2 ", base2
+        if top_dir in allowed_top_dirs:
+            if extension == 'c':
+                modules.setdefault(module + '_' + submodule, []).append(relative_path)
+            if extension == 's':
+                asm_files.setdefault(module + '_' + submodule, []).append(relative_path)
+            if extension == 'h':
+                includes[module + '_' + submodule] = os.path.dirname(relative_path)
+
+    # pp.pprint(modules)
+    # pp.pprint(includes)
+    print "# Automatically generated file"
+    for k in set(modules.keys() + includes.keys()):
+        print 'ifeq ($(SDK_' + k.upper() + '),yes)'
+        if k in modules:
+            s = ['C_SOURCE_FILES += ' + x for x in modules[k]]
+            print '\n'.join(s)
+        if k in includes:
+            print "INC_PATHS += -I", includes[k]
+
+        if k in asm_files:
+            s = ['SRCS_AS += ' + x for x in asm_files[k]]
+            print '\n'.join(s)
+
+        print 'endif\n'
