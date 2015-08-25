a6cb64df202cacb819ef448bc0ad917a8d92988f
Ronan BARZIC <rbarzic@gmail.com>
2015-08-25 15:03:26 +0200

    Add gdb server invocation

--- misc/nrftools_jlink.mk
+++ misc/nrftools_jlink.mk
@@ -2,14 +2,23 @@
 # it avoid to deal directly with scripts as
 # done in nrf51-pure-gcc-setup
 
+GDB_PORT ?= 2345
+
 PROGRAMMER ?= nrftool
 
-JLINKEXE=JLinkExe
+JLINKEXE ?=JLinkExe
+JLINKGDBSERVER ?= JLinkGDBServer
+JLINKGDBSERVER_OPTIONS ?= -if SWD -port $(GDB_PORT)
 
-JLINKEXE_PATH=$(shell which $(JLINKEXE))
+
+JLINKEXE_PATH ?= $(shell which $(JLINKEXE))
 
 erase-all:
 	$(PROGRAMMER) --device=$(DEVICE) --jlinkexe $(JLINKEXE_PATH) erase
 
 program:
 	$(PROGRAMMER) --device=$(DEVICE) --jlinkexe $(JLINKEXE_PATH) flash $(PROJECT_NAME).bin $(FLASH_START_ADDRESS)
+
+jlinkgdbsever:
+	$(JLINKGDBSERVER) $(JLINKGDBSERVER_OPTIONS) &
+
