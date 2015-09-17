3dcb4a8a53287005be43d58e4f11bc000ad56983
Frank Aune <Frank.Aune@nordicsemi.no>
2015-09-17 15:05:56 +0200

    Added support for nrfjprog

--- misc/nrftools_jlink.mk
+++ misc/nrftools_jlink.mk
@@ -3,9 +3,11 @@
 # done in nrf51-pure-gcc-setup
 
 
+# Supported values are nrftool or nrfjprog
 
 PROGRAMMER ?= nrftool
 
+# JLink options
 JLINKEXE ?=JLinkExe
 JLINKGDBSERVER ?= JLinkGDBServer
 JLINKGDBSERVER_OPTIONS ?= -if SWD -port $(GDB_PORT)
@@ -14,10 +16,20 @@ JLINKGDBSERVER_OPTIONS ?= -if SWD -port $(GDB_PORT)
 JLINKEXE_PATH ?= $(shell which $(JLINKEXE))
 
 erase-all:
+ifeq ($(PROGRAMMER),nrftool)
 	$(PROGRAMMER) --device=$(DEVICE) --jlinkexe $(JLINKEXE_PATH) erase
+endif
+ifeq ($(PROGRAMMER),nrfjprog)
+	$(PROGRAMMER) --family $(DEVICE) --eraseall
+endif
 
 program:
+ifeq ($(PROGRAMMER),nrftool)
 	$(PROGRAMMER) --device=$(DEVICE) --jlinkexe $(JLINKEXE_PATH) flash $(PROJECT_NAME).bin $(FLASH_START_ADDRESS)
+endif
+ifeq ($(PROGRAMMER),nrfjprog)
+	$(PROGRAMMER) --family $(DEVICE) --program $(PROJECT_NAME).hex
+endif
 
 jlinkgdbsever:
 	$(JLINKGDBSERVER) $(JLINKGDBSERVER_OPTIONS) &
